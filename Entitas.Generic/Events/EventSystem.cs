4389c6a6ee059db3c25382524fb6a5335620159d
Mikalaj Siatkouski <mikalajsiatkouski@gmail.com>
2023-11-01 13:25:41 +0300

    Listener implementation

---
+++ Entitas.Generic/Events/EventSystem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace Entitas.Generic
+{
+	public sealed class EventSystem<TScope, TComponent> : ReactiveSystem<Entity<TScope>>
+		where TComponent : class, IComponent, IEntity, new()
+		where TScope : IScope
+	{
+		private readonly List<IListener<TScope, TComponent>> _listenerBuffer;
+
+		public EventSystem(Contexts contexts)
+			: base(contexts.Get<TScope>())
+		{
+			_listenerBuffer = new List<IListener<TScope, TComponent>>();
+		}
+
+		protected override ICollector<Entity<TScope>> GetTrigger(IContext<Entity<TScope>> context)
+			=> context.CreateCollector(Matcher<TScope>.Get<TComponent>().Added());
+
+		protected override bool Filter(Entity<TScope> entity)
+			=> entity.Has<TComponent>() && entity.Has<ListenerComponent<TScope, TComponent>>();
+
+		protected override void Execute(List<Entity<TScope>> entities)
+		{
+			foreach (var e in entities)
+			{
+				var component = e.Get<TComponent>();
+				_listenerBuffer.Clear();
+				_listenerBuffer.AddRange(e.Get<ListenerComponent<TScope, TComponent>>().Value);
+
+				foreach (var listener in _listenerBuffer)
+					listener.OnPosition(e, component);
+			}
+		}
+	}
+}
---
+++ Entitas.Generic/Events/Listener.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Entitas.Generic
+{
+	public class ListenerComponent<TScope, TComponent> : IComponent
+		where TScope : IScope
+		where TComponent : IComponent
+	{
+		public List<IListener<TScope, TComponent>> Value;
+	}
+
+	public interface IListener<TScope, in TComponent>
+		where TComponent : IComponent
+		where TScope : IScope
+	{
+		void OnPosition(Entity<TScope> entity, TComponent component);
+	}
+}
