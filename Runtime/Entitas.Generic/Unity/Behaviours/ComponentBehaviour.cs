c70c47abdae3f454ad76e2108c0e673b88848701
Kolia Siatkouski <n.siatkouski@furylion.dev>
2023-11-03 17:25:11 +0300

    +Component & Entity base behaviours classes

---
+++ Runtime/Entitas.Generic/Unity/Behaviours/ComponentBehaviour.cs
@@ -0,0 +1,46 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Entitas.Generic
+{
+	public abstract class ComponentBehaviourBase<TScope> : MonoBehaviour
+		where TScope : IScope
+	{
+		public abstract void Add(ref Entity<TScope> entity);
+
+		public abstract void Remove(ref Entity<TScope> entity);
+	}
+
+	public abstract class ComponentBehaviour<TScope, TComponent> : ComponentBehaviourBase<TScope>
+		where TScope : IScope
+		where TComponent : FlagComponent, new()
+	{
+		public override void Add(ref Entity<TScope> entity)
+		{
+			entity.Is<TComponent>(true);
+		}
+
+		public override void Remove(ref Entity<TScope> entity)
+		{
+			entity.Is<TComponent>(false);
+		}
+	}
+
+	public abstract class ComponentBehaviour<TScope, TComponent, TValue> : ComponentBehaviourBase<TScope>
+		where TScope : IScope
+		where TComponent : ValueComponent<TValue>, new()
+	{
+		[SerializeField] private TValue _value;
+
+		public override void Add(ref Entity<TScope> entity)
+		{
+			entity.Add<TComponent, TValue>(_value);
+		}
+
+		public override void Remove(ref Entity<TScope> entity)
+		{
+			entity.Remove<TComponent>();
+		}
+	}
+}
+  #endif
---
+++ Runtime/Entitas.Generic/Unity/Behaviours/EntityBehaviour.cs
@@ -0,0 +1,47 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+namespace Entitas.Generic
+{
+	public abstract class EntityBehaviour : MonoBehaviour
+	{
+		public abstract void Register();
+
+		[ContextMenu(nameof(CollectComponentsFromGameObject))]
+		public abstract void CollectComponentsFromGameObject();
+	}
+
+	public class EntityBehaviour<TScope> : EntityBehaviour
+		where TScope : IScope
+	{
+		[SerializeField] private ComponentBehaviourBase<TScope>[] _componentBehaviours;
+
+		private Entity<TScope> _entity;
+
+		public Entity<TScope> Entity => _entity;
+
+#if DEBUG
+		private void Awake()
+		{
+			var actualCount = GetComponents<ComponentBehaviourBase<TScope>>().Length;
+
+			if (actualCount != _componentBehaviours.Length)
+				Debug.LogWarning($"Not all component behaviours are added to the entity on {gameObject.name}!");
+		}
+#endif
+
+		public override void Register()
+		{
+			_entity = Contexts.Instance.Get<TScope>().CreateEntity();
+
+			foreach (var component in _componentBehaviours)
+				component.Add(ref _entity);
+		}
+
+		public override void CollectComponentsFromGameObject()
+		{
+			_componentBehaviours = GetComponents<ComponentBehaviourBase<TScope>>();
+		}
+	}
+}
+#endif
