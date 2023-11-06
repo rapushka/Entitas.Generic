43fd4cf6f2e31daec5b64bfc17ec8f347f385ba8
rapushka <mikalajsiatkouski@gmail.com>
2023-11-06 06:20:13 +0300

    component id drawer rework to generic

--- Runtime/Entitas.Generic/Unity/ComponentID/ComponentID.Drawer.cs
+++ Runtime/Entitas.Generic/Unity/ComponentID/ComponentID.Drawer.cs
@@ -1,17 +1,19 @@
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
-using static System.Reflection.BindingFlags;
 
 namespace Entitas.Generic
 {
-	[CustomPropertyDrawer(typeof(ComponentIDBase), useForChildren: true)]
-	public class ComponentIDDrawer : PropertyDrawer
+	// [CustomPropertyDrawer(typeof(ComponentIDBase), useForChildren: true)]
+	public class ComponentIDDrawer<TScope> : PropertyDrawer
+		where TScope : IScope
 	{
 		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
 		{
 			EditorGUI.BeginProperty(position, label, property);
-			var names = GetComponentNames(property);
+
+			ComponentTypeManager<TScope>.AutoScan();
+			var names = ComponentTypeManager<TScope>.ComponentNames;
 
 			var nameProperty = property.FindPropertyRelative("_name");
 			var selectedName = nameProperty.stringValue;
@@ -22,55 +24,6 @@ namespace Entitas.Generic
 			nameProperty.stringValue = names[selectedIndex];
 
 			EditorGUI.EndProperty();
-		}
-
-		private string[] GetComponentNames(SerializedProperty property)
-		{
-			var type = GetPropertyInstance(property).GetType();
-			var scopeType = type.GetGenericArguments().Single();
-			var lookupType = typeof(ComponentTypeManager<>).MakeGenericType(scopeType);
-			var autoScanMethod = lookupType.GetMethod("AutoScan", Static | Public);
-			autoScanMethod!.Invoke(null, null);
-
-			return lookupType.GetStaticField<string[]>("ComponentNames");
-		}
-
-		public object GetPropertyInstance(SerializedProperty property)
-		{
-			var path = property.propertyPath;
-
-			object @object = property.serializedObject.targetObject;
-			var type = @object.GetType();
-			var baseType = type.BaseType;
-
-			while (baseType != null)
-			{
-				foreach (var field in baseType.GetFields())
-					Debug.Log($"field = {field} {field.Name}");
-
-				baseType = baseType.BaseType;
-			}
-
-			var fieldNames = path.Split('.');
-
-			foreach (var name in fieldNames)
-			{
-				var info = type.GetField(name, NonPublic | Instance);
-				foreach (var field in type.GetFields())
-				{
-					Debug.Log($"field = {field}");
-				}
-
-				if (info == null)
-					break;
-
-				@object = info.GetValue(@object);
-				type = info.FieldType;
-			}
-
-			Debug.Log($"type = {type.Name}\n@object = {@object}");
-
-			return @object;
 		}
 	}
 }
