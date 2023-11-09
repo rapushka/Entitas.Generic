cc33c1b146ec02676e7767786cd276b6623ef023
rapushka <mikalajsiatkouski@gmail.com>
2023-11-10 01:11:05 +0300

    +Entity.ToStirng()

---
+++ Runtime/Entitas.Generic/Core/Entity/Entity.ToString.cs
@@ -0,0 +1,23 @@
+namespace Entitas.Generic
+{
+	public partial class Entity<TScope>
+	{
+		public static IEntityFormatter<TScope> Formatter { get; set; }
+
+		public string ToString<TComponent>(string prefix = "", string postfix = "", string defaultValue = "")
+			where TComponent : FlagComponent, new()
+			=> Is<TComponent>() ? $"{prefix}{typeof(TComponent).Name}{postfix}" : defaultValue;
+
+		public string ToString<TComponent, TValue>(string prefix = "", string postfix = "", string defaultValue = "")
+			where TComponent : ValueComponent<TValue>, new()
+			=> Has<TComponent>() ? $"{prefix}{Get<TComponent, TValue>().ToString()}{postfix}" : defaultValue;
+
+		public override string ToString() => Formatter?.ToString(this) ?? base.ToString();
+	}
+
+	public interface IEntityFormatter<TScope>
+		where TScope : IScope
+	{
+		string ToString(Entity<TScope> entity);
+	}
+}
