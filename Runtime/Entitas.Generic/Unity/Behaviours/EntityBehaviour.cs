3e2edb2445a1a339de6cfd62c384c451454fe813
rapushka <mikalajsiatkouski@gmail.com>
2023-11-11 01:08:28 +0300

    fix behaviour creation

--- Runtime/Entitas.Generic/Unity/Behaviours/EntityBehaviour.cs
+++ Runtime/Entitas.Generic/Unity/Behaviours/EntityBehaviour.cs
@@ -1,10 +1,12 @@
-﻿using UnityEngine;
+﻿using JetBrains.Annotations;
+using UnityEngine;
 
 namespace Entitas.Generic
 {
 	public abstract class EntityBehaviour : MonoBehaviour
 	{
-		public abstract void Register(Contexts contexts);
+		public abstract void CreateEntity(Contexts contexts);
+		public abstract void Register();
 
 		public abstract void CollectComponents();
 	}
@@ -16,6 +18,7 @@ namespace Entitas.Generic
 
 		private Entity<TScope> _entity;
 
+		[PublicAPI]
 		public Entity<TScope> Entity => _entity;
 
 		protected override ComponentBehaviourBase<TScope>[] ComponentBehaviours
@@ -24,10 +27,13 @@ namespace Entitas.Generic
 			set => _componentBehaviours = value;
 		}
 
-		public override void Register(Contexts contexts)
+		public override void CreateEntity(Contexts contexts)
 		{
 			_entity = contexts.Get<TScope>().CreateEntity();
+		}
 
+		public override void Register()
+		{
 			foreach (var component in ComponentBehaviours)
 				component.Add(ref _entity);
 		}
