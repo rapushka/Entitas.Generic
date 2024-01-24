5ae8eba8b7b862533c2b48860c836b783b3800a4
Rapuska <mikalajsiatkouski@gmail.com>
2024-01-24 10:14:00 +0300

    All extension methods become internal

--- Editor/Entitas.Generic/Extensions/GUILayoutExtensions.cs
+++ Editor/Entitas.Generic/Extensions/GUILayoutExtensions.cs
@@ -4,20 +4,20 @@ using UnityEngine;
 
 namespace Entitas.Generic
 {
-	public static class GUILayoutExtensions
+	internal static class GUILayoutExtensions
 	{
-		public static void OnClick(this bool @this, Action action)
+		internal static void OnClick(this bool @this, Action action)
 		{
 			if (@this)
 				action.Invoke();
 		}
 
-		public static void GuiToggle(this ref bool @this, string text)
+		internal static void GuiToggle(this ref bool @this, string text)
 		{
 			@this = GUILayout.Toggle(@this, text);
 		}
 
-		public static void GuiField(this SerializedProperty @this, string text)
+		internal static void GuiField(this SerializedProperty @this, string text)
 		{
 			EditorGUILayout.PropertyField(@this, new GUIContent(text));
 		}
--- Runtime/Entitas.Generic/Utils/ArrayExtensions.cs
+++ Runtime/Entitas.Generic/Utils/ArrayExtensions.cs
@@ -2,9 +2,9 @@ using System;
 
 namespace Entitas.Generic
 {
-	public static class ArrayExtensions
+	internal static class ArrayExtensions
 	{
-		public static int IndexOf<T>(this T[] @this, T item, bool clamp = false)
+		internal static int IndexOf<T>(this T[] @this, T item, bool clamp = false)
 		{
 			var index = Array.IndexOf(@this, item);
 			return clamp ? Math.Clamp(index, min: 0, max: @this.Length) : index;
