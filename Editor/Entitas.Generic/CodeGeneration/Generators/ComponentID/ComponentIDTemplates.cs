f2588716bd085ba3b172c30b48bcff6e4c7c30a2
Rapuska <mikalajsiatkouski@gmail.com>
2024-03-24 12:03:41 +0300

    +ComponentIDDrawerGenerator

--- Editor/Entitas.Generic/CodeGeneration/Generators/ComponentID/ComponentIDTemplates.cs
+++ Editor/Entitas.Generic/CodeGeneration/Generators/ComponentID/ComponentIDTemplates.cs
@@ -4,7 +4,14 @@ namespace Entitas.Generic
 {
 	internal static class ComponentIDTemplates
 	{
-		internal static string ComponentID(Type scopeType)
-			=> $"\tpublic class {scopeType.Name}ComponentID : Entitas.Generic.ComponentID<{scopeType.FullName}> {{ }}";
+		internal static string ComponentID(Type scope)
+			=> $"\tpublic class {scope.Name}ComponentID : {BaseComponentID(scope)} {{ }}";
+
+		internal static string PropertyDrawer(Type scope)
+			=> $"\t[UnityEditor.CustomPropertyDrawer(typeof({BaseComponentID(scope)}), true)]\n"
+			   + $"\tpublic class {scope.Name}ComponentIDDrawer "
+			   + $": Entitas.Generic.ComponentIDDrawer<{scope.FullName}> {{ }}";
+
+		private static string BaseComponentID(Type scope) => $"Entitas.Generic.ComponentID<{scope.FullName}>";
 	}
 }
