e660523a705795bd953365cda162ec1eb1d8ca90
Rapuska <mikalajsiatkouski@gmail.com>
2024-04-21 11:20:44 +0300

    +Container.Replace API

---
+++ Runtime/Unique/Container/Container.Replace.cs
@@ -0,0 +1,13 @@
+namespace Entitas.Generic;
+
+public partial class UniqueComponentsContainer<TScope>
+{
+	public void Replace<TComponent>(Entity<TScope> newUnique)
+		where TComponent : FlagComponent, IInScope<TScope>, IUnique, new()
+	{
+		var oldUnique = GetEntityOrDefault<TComponent>();
+		oldUnique?.Is<TComponent>(false);
+
+		newUnique.Is<TComponent>(true);
+	}
+}
