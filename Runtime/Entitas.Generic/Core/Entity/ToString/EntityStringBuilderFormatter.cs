277559a6b8c174485d46ef313a7ef2b25718f267
rapuska <mikalajsiatkouski@gmail.com>
2024-11-03 00:04:36 +0300

    fix

--- Runtime/Entitas.Generic/Core/Entity/ToString/EntityStringBuilderFormatter.cs
+++ Runtime/Entitas.Generic/Core/Entity/ToString/EntityStringBuilderFormatter.cs
@@ -5,12 +5,16 @@ namespace Entitas.Generic
     public abstract class EntityStringBuilderFormatter<TScope> : IEntityFormatter<TScope>
         where TScope : IScope
     {
+        private StringBuilder _stringBuilder;
+
         public string ToString(Entity<TScope> entity)
         {
-            var stringBuilder = new StringBuilder();
-            BuildName(ref stringBuilder, entity);
+            _stringBuilder ??= new StringBuilder();
+            _stringBuilder.Clear();
 
-            return stringBuilder.ToString();
+            BuildName(ref _stringBuilder, entity);
+
+            return _stringBuilder.ToString();
         }
 
         protected abstract void BuildName(ref StringBuilder stringBuilder, in Entity<TScope> entity);
