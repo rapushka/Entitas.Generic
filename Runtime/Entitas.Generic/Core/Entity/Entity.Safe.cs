b9e80c267aff1eea4b0714b39f8d9159e5329585
rapuska <mikalajsiatkouski@gmail.com>
2024-11-03 11:09:14 +0300

    overload

--- Runtime/Entitas.Generic/Core/Entity/Entity.Safe.cs
+++ Runtime/Entitas.Generic/Core/Entity/Entity.Safe.cs
@@ -2,37 +2,47 @@ using JetBrains.Annotations;
 
 namespace Entitas.Generic
 {
-	public partial class Entity<TScope>
-	{
-		[PublicAPI]
-		[CanBeNull]
-		public TComponent GetOrDefault<TComponent>()
-			where TComponent : IComponent, IInScope<TScope>, new()
-			=> Has<TComponent>() ? Get<TComponent>() : default;
+    public partial class Entity<TScope>
+    {
+        [PublicAPI]
+        [CanBeNull]
+        public TComponent GetOrDefault<TComponent>()
+            where TComponent : IComponent, IInScope<TScope>, new()
+            => Has<TComponent>() ? Get<TComponent>() : default;
 
-		[PublicAPI]
-		public TValue GetOrDefault<TComponent, TValue>(TValue defaultValue = default)
-			where TComponent : ValueComponent<TValue>, IInScope<TScope>, new()
-			=> Has<TComponent>() ? Get<TComponent>().Value : defaultValue;
+        [PublicAPI]
+        public TValue GetOrDefault<TComponent, TValue>(TValue defaultValue = default)
+            where TComponent : ValueComponent<TValue>, IInScope<TScope>, new()
+            => Has<TComponent>() ? Get<TComponent>().Value : defaultValue;
 
-		[PublicAPI]
-		public Entity<TScope> AddSafely<TComponent>()
-			where TComponent : IComponent, IInScope<TScope>, new()
-		{
-			if (!Has<TComponent>())
-				Add<TComponent>();
+        [PublicAPI]
+        public Entity<TScope> AddSafely<TComponent>()
+            where TComponent : IComponent, IInScope<TScope>, new()
+        {
+            if (!Has<TComponent>())
+                Add<TComponent>();
 
-			return this;
-		}
+            return this;
+        }
 
-		[PublicAPI]
-		public Entity<TScope> RemoveSafely<TComponent>()
-			where TComponent : IComponent, IInScope<TScope>, new()
-		{
-			if (Has<TComponent>())
-				Remove<TComponent>();
+        [PublicAPI]
+        public Entity<TScope> AddSafely<TComponent, TValue>(TValue value)
+            where TComponent : ValueComponent<TValue>, IInScope<TScope>, new()
+        {
+            if (!Has<TComponent>())
+                Add<TComponent, TValue>(value);
 
-			return this;
-		}
-	}
+            return this;
+        }
+
+        [PublicAPI]
+        public Entity<TScope> RemoveSafely<TComponent>()
+            where TComponent : IComponent, IInScope<TScope>, new()
+        {
+            if (Has<TComponent>())
+                Remove<TComponent>();
+
+            return this;
+        }
+    }
 }
