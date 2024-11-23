7e1f6c5624a81b215a49d681879e66a6e270fea7
rapuska <mikalajsiatkouski@gmail.com>
2024-11-23 13:16:47 +0300

    sort components by name

--- Runtime/Entitas.Generic/Core/Lookup/ComponentsLookup.cs
+++ Runtime/Entitas.Generic/Core/Lookup/ComponentsLookup.cs
@@ -4,70 +4,74 @@ using System.Linq;
 
 namespace Entitas.Generic
 {
-	public class ComponentsLookup<TScope>
-		where TScope : IScope
-	{
-		private readonly List<Type> _componentTypes = new();
+    public class ComponentsLookup<TScope>
+        where TScope : IScope
+    {
+        private readonly List<Type> _componentTypes = new();
 
-		private int _lastComponentIndex;
-		private bool _initialized;
+        private int _lastComponentIndex;
+        private bool _initialized;
 
-		private static ComponentsLookup<TScope> _instance;
-		public static ComponentsLookup<TScope> Instance => _instance ??= new ComponentsLookup<TScope>().Initialize();
+        private static ComponentsLookup<TScope> _instance;
+        public static ComponentsLookup<TScope> Instance => _instance ??= new ComponentsLookup<TScope>().Initialize();
 
-		private ComponentsLookup() { }
+        private ComponentsLookup() { }
 
-		public string[] ComponentNames { get; private set; }
+        public string[] ComponentNames { get; private set; }
 
-		public Type[] ComponentTypes { get; private set; }
+        public Type[] ComponentTypes { get; private set; }
 
-		public int TotalComponents => _componentTypes.Count;
+        public int TotalComponents => _componentTypes.Count;
 
-		private static IEnumerable<Type> AllTypes
-			=> AppDomain.CurrentDomain.GetAssemblies().SelectMany((a) => a.GetTypes());
+        private static IEnumerable<Type> AllTypes
+            => AppDomain.CurrentDomain.GetAssemblies().SelectMany((a) => a.GetTypes())
+#if UNITY_EDITOR
+                        .OrderBy(t => t.Name)
+#endif
+        ;
 
-		public ComponentsLookup<TScope> Initialize()
-		{
-			if (_initialized)
-				return this;
+        public ComponentsLookup<TScope> Initialize()
+        {
+            if (_initialized)
+                return this;
 
-			RegisterAllTypes();
+            RegisterAllTypes();
 
-			ComponentTypes = _componentTypes.ToArray();
-			ComponentNames = ComponentTypes.Select((x) => x.Name).ToArray();
+            ComponentTypes = _componentTypes.ToArray();
+            ComponentNames = ComponentTypes.Select((x) => x.Name).ToArray();
 
-			_initialized = true;
+            _initialized = true;
 
-			return this;
-		}
+            return this;
+        }
 
-		private void RegisterAllTypes()
-		{
-			foreach (var type in AllTypes)
-			{
-				if (!type.IsDerivedFrom<IInScope<TScope>>())
-					continue;
+        private void RegisterAllTypes()
+        {
+            foreach (var type in AllTypes)
+            {
+                if (!type.IsDerivedFrom<IInScope<TScope>>())
+                    continue;
 
-				if (type.IsDerivedFrom<IComponent>())
-					Register(type);
+                if (type.IsDerivedFrom<IComponent>())
+                    Register(type);
 
-				if (type.IsDerivedFrom<IEvent>())
-					RegisterListener(type);
-			}
-		}
+                if (type.IsDerivedFrom<IEvent>())
+                    RegisterListener(type);
+            }
+        }
 
-		private void RegisterListener(Type componentType)
-			=> Register(typeof(ListenerComponent<,>).MakeGenericType(typeof(TScope), componentType));
+        private void RegisterListener(Type componentType)
+            => Register(typeof(ListenerComponent<,>).MakeGenericType(typeof(TScope), componentType));
 
-		private void Register(Type componentType)
-		{
-			var indexType = typeof(ComponentIndex<,>).MakeGenericType(typeof(TScope), componentType);
+        private void Register(Type componentType)
+        {
+            var indexType = typeof(ComponentIndex<,>).MakeGenericType(typeof(TScope), componentType);
 
-			if (!_componentTypes.Contains(componentType))
-			{
-				_componentTypes.Add(componentType);
-				indexType.SetStaticField("Value", _lastComponentIndex++);
-			}
-		}
-	}
+            if (!_componentTypes.Contains(componentType))
+            {
+                _componentTypes.Add(componentType);
+                indexType.SetStaticField("Value", _lastComponentIndex++);
+            }
+        }
+    }
 }
